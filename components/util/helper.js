5a451ac5ab23a6a71b47c1e8b66b0dd1b36d5b64
Javi Velasco <javier.velasco86@gmail.com>
2015-09-05 02:10:47 +0200

    Disable linting of parameter reassignation

--- components/util/helper.js
+++ components/util/helper.js
@@ -1,16 +1,12 @@
 module.exports = {
 
-  range (start, stop, step = 1) {
-    if (stop == null) {
-      stop = start || 0;
-      start = 0;
-    }
-
-    let length = Math.max(Math.ceil((stop - start) / step), 0);
+  range (start = 0, stop = null, step = 1) {
+    let [_start, _stop] = (stop !== null) ? [start, stop] : [0, start];
+    let length = Math.max(Math.ceil((_stop - _start) / step), 0);
     let range = Array(length);
 
-    for (const idx = 0; idx < length; idx++, start += step) {
-      range[idx] = start;
+    for (let idx = 0; idx < length; idx++, _start += step) {
+      range[idx] = _start;
     }
 
     return range;
