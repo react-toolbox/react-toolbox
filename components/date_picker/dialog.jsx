0960527914fd8ae632fa5520478bb040fb6f92de
Javi Velasco <javier.velasco86@gmail.com>
2015-09-07 01:59:11 +0200

    Move date picker to ES6

---
+++ components/date_picker/dialog.jsx
@@ -0,0 +1,85 @@
+const React = window.React;
+const css = require('./style');
+const time = require('../utils/time');
+
+const Calendar = require('../calendar');
+const Dialog = require('../dialog');
+
+module.exports = React.createClass({
+  displayName: 'CalendarDialog',
+
+  propTypes: {
+    initialDate: React.PropTypes.object,
+    onDateSelected: React.PropTypes.func
+  },
+
+  getDefaultProps () {
+    return {
+      initialDate: new Date()
+    };
+  },
+
+  getInitialState () {
+    return {
+      date: this.props.initialDate,
+      display: 'months',
+      actions: [
+        { caption: 'Cancel', type: 'flat accent', onClick: this.onDateCancel },
+        { caption: 'Ok', type: 'flat accent', onClick: this.onDateSelected }
+      ]
+    };
+  },
+
+  onCalendarChange (date) {
+    this.setState({date: date, display: 'months'});
+  },
+
+  onDateCancel () {
+    this.refs.dialog.hide();
+  },
+
+  onDateSelected () {
+    if (this.props.onDateSelected) this.props.onDateSelected(this.state.date);
+    this.refs.dialog.hide();
+  },
+
+  show () {
+    this.refs.dialog.show();
+  },
+
+  displayMonths () {
+    this.setState({display: 'months'});
+  },
+
+  displayYears () {
+    this.setState({display: 'years'});
+  },
+
+  render () {
+    const className = `display-${this.state.display}`;
+    return (
+      <Dialog ref="dialog" className={className} type={css.dialog} actions={this.state.actions}>
+          <header className={css.header}>
+            <span className={css.headerWeekday}>
+              {time.getFullDayOfWeek(this.state.date.getDay())}
+            </span>
+            <div onClick={this.displayMonths}>
+              <span className={css.headerMonth}>{time.getShortMonth(this.state.date)}</span>
+              <span className={css.headerDay}>{this.state.date.getDate()}</span>
+            </div>
+            <span className={css.headerYear} onClick={this.displayYears}>
+              {this.state.date.getFullYear()}
+            </span>
+          </header>
+
+          <div className={css.calendarWrapper}>
+            <Calendar
+              ref="calendar"
+              display={this.state.display}
+              onChange={this.onCalendarChange}
+              selectedDate={this.state.date} />
+          </div>
+      </Dialog>
+    );
+  }
+});
---
+++ components/date_picker/index.jsx
@@ -0,0 +1,61 @@
+const React = window.React;
+const time = require('../utils/time');
+
+const CalendarDialog = require('./dialog');
+const Input = require('../input');
+
+module.exports = React.createClass({
+  displayName: 'DatePicker',
+
+  propTypes: {
+    className: React.PropTypes.string,
+    value: React.PropTypes.object
+  },
+
+  getDefaultProps () {
+    return {
+      className: ''
+    };
+  },
+
+  getInitialState () {
+    return {
+      value: this.props.value
+    };
+  },
+
+  openCalendarDialog () {
+    this.refs.dialog.show();
+  },
+
+  onDateSelected (value) {
+    this.refs.input.setValue(this.formatDate(value));
+    this.setState({value: value});
+  },
+
+  formatDate (date) {
+    return `${date.getDate()} ${time.getFullMonth(date)} ${date.getFullYear()}`;
+  },
+
+  getValue () {
+    return this.state.value;
+  },
+
+  render () {
+    return (
+      <div>
+        <Input
+            ref="input"
+            type="text"
+            disabled={true}
+            onClick={this.openCalendarDialog}
+            placeholder="Pick up date"
+            value={this.state.value ? this.formatDate(this.state.value) : null} />
+        <CalendarDialog
+            ref="dialog"
+            initialDate={this.state.value}
+            onDateSelected={this.onDateSelected} />
+      </div>
+    );
+  }
+});
