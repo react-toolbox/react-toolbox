b206ab793cdc53045ed87b1e56e21585027b22ba
Javi Velasco <javier.velasco86@gmail.com>
2015-09-22 09:25:15 +0200

    Add React hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   module: {
     noParse: [node_modules + '/react/dist/*.js'],
     loaders: [
-      { test: /(\.js|\.jsx)$/, exclude: /(node_modules)/, loader: 'babel?optional=runtime' },
+      { test: /(\.js|\.jsx)$/, exclude: /(node_modules)/, loaders: ['react-hot', 'babel'] },
       { test: /\.styl$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!stylus-loader!') }
     ]
   },
