9a5b466eef207c503937cf06c8c3b2068571e042
@soyjavi <javi.jimenez.villar@gmail.com>
2015-10-05 21:18:02 +0700

    New types "image" "event"

--- spec/components/card.jsx
+++ spec/components/card.jsx
@@ -26,15 +26,16 @@ export default React.createClass({
         <h3>Basic properties</h3>
         <Card title='Default Card' />
         <Card title='Default Card loading' loading />
+        <Card type='wide' title='Wide card' />
         <Card title='Default Card with text' text={text} />
         <Card title='Default Card with actions' actions={actions} />
         <Card title='Default Card with text and image' text={text} image='https://avatars2.githubusercontent.com/u/559654?v=3&s=460' />
         <Card title='Default Card with text, image and color' text={text} color='#e91e63' image='https://avatars2.githubusercontent.com/u/559654?v=3&s=460' />
         <Card title='Default Card with text, image and color' text={text} color='#00bcd4' image='https://avatars2.githubusercontent.com/u/1634922?v=3&s=460' />
         <Card title='Default Card with text, color and onClick event' text={text} color='#e91e63' onClick={this.onClick} />
-        <Card title='Default Card loading with text, color and onClick event' text={text} color='#e91e63' onClick={this.onClick} loading />
-        <Card type='square' title='Wide card with text and onClick event' text={text} color='#00bcd4' onClick={this.onClick} />
-        <Card type='wide' title='Wide card with text and onClick event' text={text} color='#00bcd4' onClick={this.onClick} />
+        <Card type='wide' title='Wide Card loading with text, color and onClick event' text={text} color='#e91e63' onClick={this.onClick} loading />
+        <Card type='image' title='javivelasco.jpg' image='https://avatars2.githubusercontent.com/u/1634922?v=3&s=460' />
+        <Card type='event' title='Featured event: May 24, 2016 7-11pm' color='#00bcd4' />
       </section>
     );
   }
