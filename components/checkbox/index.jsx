87265577f200e0c8b08516de28946ed56e1a43aa
Javi Velasco <javier.velasco86@gmail.com>
2015-10-05 09:12:16 +0200

    Add checkbox component

---
+++ components/checkbox/index.jsx
@@ -0,0 +1,86 @@
+/* global React */
+
+import { addons } from 'react/addons';
+import Ripple from '../ripple';
+import style from './style.scss';
+import events from '../utils/events';
+
+export default React.createClass({
+  mixins: [addons.PureRenderMixin],
+
+  displayName: 'Checkbox',
+
+  propTypes: {
+    checked: React.PropTypes.bool,
+    className: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    label: React.PropTypes.string,
+    onBlur: React.PropTypes.func,
+    onChange: React.PropTypes.func,
+    onFocus: React.PropTypes.func
+  },
+
+  getDefaultProps () {
+    return {
+      className: '',
+      disabled: false
+    };
+  },
+
+  getInitialState () {
+    return { checked: this.props.checked };
+  },
+
+  handleChange (event) {
+    this.setState({checked: !this.state.checked});
+    if (this.props.onChange) this.props.onChange(event, this);
+  },
+
+  handleClick (event) {
+    events.pauseEvent(event);
+    if (!this.props.disabled) this.handleChange(event);
+  },
+
+  handleMouseDown (event) {
+    if (!this.props.disabled) this.refs.ripple.start(event);
+  },
+
+  render () {
+    let labelClassName = style[this.props.disabled ? 'disabled' : 'field'];
+    if (this.props.className) labelClassName += ` ${this.props.className}`;
+    let checkboxClassName = style[this.state.checked ? 'checked' : 'check'];
+
+    return (
+      <label react-toolbox='checkbox' className={labelClassName} onClick={this.handleClick}>
+        <input
+          {...this.props}
+          ref='input'
+          type='checkbox'
+          className={style.input}
+          onChange={this.handleChange}
+          checked={this.state.checked}
+        />
+        <span role='checkbox' className={checkboxClassName} onMouseDown={this.handleMouseDown}>
+          <Ripple ref='ripple' role='ripple' className={style.ripple} spread={3} centered />
+        </span>
+        { this.props.label ? <span className={style.text}>{this.props.label}</span> : null }
+      </label>
+    );
+  },
+
+  blur () {
+    this.refs.input.getDOMNode().blur();
+  },
+
+  focus () {
+    this.refs.input.getDOMNode().focus();
+  },
+
+  getValue () {
+    return this.state.checked;
+  },
+
+  setValue (value) {
+    this.setState({checked: value});
+  }
+});
---
+++ spec/components/checkbox.jsx
@@ -0,0 +1,47 @@
+/* global React */
+
+import Checkbox from '../../components/checkbox';
+
+export default React.createClass({
+  handleChange () {
+    console.log('Changed!');
+  },
+
+  handleFocus () {
+    console.log('Focused');
+  },
+
+  handleBlur () {
+    console.log('Blur');
+  },
+
+  render () {
+    return (
+      <section>
+        <h2>Checkbox</h2>
+        <p style={{marginBottom: '10px'}}>Lorem ipsum...</p>
+        <Checkbox
+          label="Checked checkbox"
+          checked
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
+        <Checkbox
+          label="Not checked biatch"
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
+        <Checkbox
+          label="Disabled checkbox"
+          checked
+          disabled
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
+      </section>
+    );
+  }
+});
