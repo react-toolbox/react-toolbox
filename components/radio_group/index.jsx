bdeda85d52f86ceaf02b9c776bcb927337b38434
@soyjavi <javi.jimenez.villar@gmail.com>
2015-10-20 22:36:08 +0700

    <RadioButton> is not a public component.

--- components/radio_group/index.jsx
+++ components/radio_group/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import RadioButton from '../radio_button';
+import RadioButton from './radio_button';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
