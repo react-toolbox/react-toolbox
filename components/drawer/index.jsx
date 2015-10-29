ba5432adfef8377a74544a3c68c34a1828836044
Javi Velasco <javier.velasco86@gmail.com>
2015-10-30 00:49:46 +0100

    Add a drawer panel with playground area

--- components/drawer/index.jsx
+++ components/drawer/index.jsx
@@ -11,6 +11,7 @@ class Drawer extends React.Component {
 
   static defaultProps = {
     className: '',
+    hideable: true,
     type: 'left'
   };
 
--- docs/app/components/layout/main/appbar.jsx
+++ docs/app/components/layout/main/appbar.jsx
@@ -3,28 +3,32 @@ import { AppBar, Button } from 'react-toolbox';
 import { Link } from 'react-router';
 import Logo from '../../logo';
 import Navigation from '../../navigation';
+import PlaygroundArea from './playground_area';
 import style from './appbar.scss';
 
-const handlerPlayGroundClick = () => {
-  window.location = '/#/playground';
-};
+class MainAppBar extends React.Component {
+  handlerPlayGroundClick = () => {
+    this.refs.playground.show();
+  }
 
-const MainAppBar = () => {
-  return (
-    <AppBar className={style.appbar} flat fixed>
-      <Link to='/' className={style.brand}>
-        <Logo className={style.logo} /> React Toolbox
-      </Link>
-      <Navigation className={style.navigation}/>
-      <Button
-        accent
-        className={style['playground-button']}
-        icon='code'
-        kind='floating'
-        onClick={handlerPlayGroundClick}
-      />
-    </AppBar>
-  );
-};
+  render () {
+    return (
+      <AppBar className={style.appbar} flat fixed>
+        <Link to='/' className={style.brand}>
+          <Logo className={style.logo} /> React Toolbox
+        </Link>
+        <Navigation className={style.navigation}/>
+        <Button
+          accent
+          className={style['playground-button']}
+          icon='code'
+          kind='floating'
+          onClick={this.handlerPlayGroundClick}
+        />
+        <PlaygroundArea ref='playground' />
+      </AppBar>
+    );
+  }
+}
 
 export default MainAppBar;
