ffd7fd5078923edd8dadb03d250a6c25cdc01d6e
@soyjavi <javi.jimenez.villar@gmail.com>
2015-10-29 14:14:36 +0700

    Minor changes for playground

--- docs/app/components/layout/main/playground.jsx
+++ docs/app/components/layout/main/playground.jsx
@@ -2,11 +2,15 @@
 import React from 'react';
 import Playground from '../../playground';
 import code from '../../../examples/example.txt';
+import style from './style';
 
 const PlaygroundArea = () => {
   return (
-    <section>
-      <p>Here should be the playground</p>
+    <section className={style.documentation}>
+      <h1>Playground</h1>
+      <p>
+        lorem ipsum...
+      </p>
       <Playground codeText={code} />
     </section>
   );
