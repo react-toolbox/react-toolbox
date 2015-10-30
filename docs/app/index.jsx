19a49903532dc91ee8f1714de4c799609dfb38ca
Javi Velasco <javier.velasco86@gmail.com>
2015-10-30 10:40:55 +0100

    Restructure docs index

--- docs/app/index.jsx
+++ docs/app/index.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from './history';
-import Docs from './docs';
+import { Router, Route, IndexRoute } from 'react-router';
+import { App } from 'react-toolbox';
+import { createHashHistory } from 'history';
 
-ReactDOM.render(<Docs history={history}/>, document.getElementById('app'));
+import Home from './components/layout/home';
+import Main from './components/layout/main';
+import Playground from './components/layout/playground';
+import Documentation from './components/layout/main/components/documentation';
+
+ReactDOM.render((
+  <Router history={createHashHistory()}>
+    <Route component={App}>
+      <Route path="/" component={Home} />
+      <Route component={Main}>
+        <Route path="/components/:component" component={Documentation} />
+        <Route path="/playground" component={Playground} />
+      </Route>
+      <IndexRoute component={Home}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
