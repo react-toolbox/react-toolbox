2f489eb1d7d96cc27bc35fa71874397b1e3dd708
@soyjavi <javi.jimenez.villar@gmail.com>
2015-11-01 20:56:21 +0700

    Authors data for <Card>

---
+++ docs/app/components/layout/home/modules/authors.js
@@ -0,0 +1,16 @@
+export default {
+  javivelasco: {
+    actions: [],
+    image: 'https://pbs.twimg.com/profile_images/459485216499720192/ufS4YGOY_400x400.png',
+    text: 'Software gardener • Film, music & comic lover • Any biographer in the room?',
+    title: 'Javi Velasco',
+    subtitle: 'javivelasco'
+  },
+  soyjavi: {
+    actions: [],
+    image: 'http://soyjavi.com/assets/images/soyjavi_avatar.jpg',
+    text: 'Creative Doer · A complicated #human who builds stuff · #author · #opensource #code lover · #traveller · with a dark past being CEO & CTO',
+    title: 'Javi Jimenez',
+    subtitle: 'soyjavi'
+  }
+};
