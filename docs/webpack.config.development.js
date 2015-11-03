f3b70a5962b7cb16597b18429713fee965071d31
Javi Velasco <javier.velasco86@gmail.com>
2015-11-03 09:31:17 +0100

    Add theme configuration for docs

--- docs/webpack.config.development.js
+++ docs/webpack.config.development.js
@@ -35,7 +35,7 @@ module.exports = {
         loader: 'babel'
       }, {
         test: /(\.scss|\.css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
       }, {
         test: /(\.txt)$/,
         loader: 'raw',
