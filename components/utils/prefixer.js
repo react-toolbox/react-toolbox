8c682c32d06189e9ac4a3a87b566e195d6d66e7a
Javi Velasco <javier.velasco86@gmail.com>
2015-11-05 02:14:21 +0100

    Fixes #45 removing Object.assign and avoiding to include a polyfill

--- components/utils/prefixer.js
+++ components/utils/prefixer.js
@@ -16,12 +16,21 @@ function getPrefixes (property, value) {
   }, {});
 }
 
-function prefixer (style) {
-  let _style = style;
+function addPrefixesTo (style, property, value) {
+  const vendor = getPrefixes(property, value);
+  for (const prefix in vendor) {
+    style[prefix] = vendor[prefix];
+  }
 
-  for (const property in properties) {
-    if (style[property]) {
-      _style = Object.assign(_style, getPrefixes(property, style[property]));
+  return style;
+}
+
+function prefixer (style) {
+  const _style = {};
+  for (const property in style) {
+    _style[property] = style[property];
+    if (properties[property]) {
+      addPrefixesTo(_style, property, style[property]);
     }
   }
 
