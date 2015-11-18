e0a4168c794f196cd6afad41a3e695b2c6c9c91a
Javi Velasco <javier.velasco86@gmail.com>
2015-11-18 09:57:46 +0100

    Fix examples for tab

--- components/tabs/readme.md
+++ components/tabs/readme.md
@@ -6,15 +6,35 @@
 ```jsx
 import {Tab, Tabs} from 'react-toolbox';
 
-const TabsExample = () => (
-  <Tabs>
-    <Tab label='Primary'><small>Primary content</small></Tab>
-    <Tab label='Secondary'><small>Secondary content</small></Tab>
-    <Tab label='Third' disabled><small>Disabled content</small></Tab>
-    <Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
-    <Tab label='Fifth'><small>Fifth content</small></Tab>
-  </Tabs>
-);
+class TabsTest extends React.Component {
+  state = {
+    index: 1
+  };
+
+  handleTabChange = (index) => {
+    this.setState({index});
+  };
+
+  handleActive = () => {
+    console.log('Special one activated');
+  };
+
+  render () {
+    return (
+      <section>
+        <h5>Tabs</h5>
+        <p>This tabs can be disabled or hidden</p>
+        <Tabs index={this.state.index} onChange={this.handleTabChange}>
+          <Tab label='Primary'><small>Primary content</small></Tab>
+          <Tab label='Secondary' onActive={this.handleActive}><small>Secondary content</small></Tab>
+          <Tab label='Third' disabled><small>Disabled content</small></Tab>
+          <Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
+          <Tab label='Fifth'><small>Fifth content</small></Tab>
+        </Tabs>
+      </section>
+    );
+  }
+}
 ```
 
 ## Tabs
--- spec/components/tabs.jsx
+++ spec/components/tabs.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tabs, Tab } from '../../components/tabs';
 
 class TabsTest extends React.Component {
-
   state = {
     index: 1
   };
@@ -20,27 +19,12 @@ class TabsTest extends React.Component {
       <section>
         <h5>Tabs</h5>
         <p>This tabs can be disabled or hidden</p>
-
         <Tabs index={this.state.index} onChange={this.handleTabChange}>
-          <Tab label='Primary'>
-            <small>Primary content</small>
-          </Tab>
-
-          <Tab label='Secondary' onActive={this.handleActive}>
-            <small>Secondary content</small>
-          </Tab>
-
-          <Tab label='Third' disabled>
-            <small>Disabled content</small>
-          </Tab>
-
-          <Tab label='Fourth' hidden>
-            <small>Fourth content hidden</small>
-          </Tab>
-
-          <Tab label='Fifth'>
-            <small>Fifth content</small>
-          </Tab>
+          <Tab label='Primary'><small>Primary content</small></Tab>
+          <Tab label='Secondary' onActive={this.handleActive}><small>Secondary content</small></Tab>
+          <Tab label='Third' disabled><small>Disabled content</small></Tab>
+          <Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
+          <Tab label='Fifth'><small>Fifth content</small></Tab>
         </Tabs>
       </section>
     );
