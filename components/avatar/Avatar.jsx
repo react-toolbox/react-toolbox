a830c935a1e89432e00e587210b13bc94c56264b
Javi Velasco <javier.velasco86@gmail.com>
2015-11-27 22:09:18 +0100

    Better avatar component

--- components/avatar/Avatar.jsx
+++ components/avatar/Avatar.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import ClassNames from 'classnames';
 import FontIcon from '../font_icon';
 import style from './style';
 
 const Avatar = ({children, className, icon, image, title, ...other}) => {
-  const classes = ClassNames(style.avatar, [], className);
-  const avatarStyle = {backgroundImage: typeof image === 'string' ? `url(${image})` : null};
-
   return (
-    <div className={classes} {...other}>
-      {typeof icon === 'string' ? <FontIcon className={style.letter} value={icon} /> : icon}
-      {typeof image === 'string' && title ? <span className={style.letter}>{title[0]}</span> : null}
-      {typeof image === 'string' ? <span className={style.image} style={avatarStyle} /> : null}
+    <div data-react-toolbox='avatar' className={`${style.avatar} ${className}`} {...other}>
       {children}
+      {typeof image === 'string' ? <img className={style.image} src={image} title={title} /> : image}
+      {typeof icon === 'string' ? <FontIcon className={style.letter} value={icon} /> : icon}
+      {title ? <span className={style.letter}>{title[0]}</span> : null}
     </div>
   );
 };
--- spec/components/avatar.jsx
+++ spec/components/avatar.jsx
@@ -5,11 +5,12 @@ import GithubIcon from './github_icon';
 const AvatarTest = () => (
   <section>
     <h5>Avatars</h5>
-    <Avatar image="https://placeimg.com/80/80/animals" />
-    <Avatar title="Javier Velasco" image="https://javivelasco.com/avatar404" />
-    <Avatar icon="folder" />
-    <Avatar><GithubIcon /></Avatar>
-    <Avatar><img src="https://placeimg.com/80/80/animals" /></Avatar>
+    <p>Provide an image source or object, a font icon, children or a title to use its first letter.</p>
+    <Avatar style={{backgroundColor: 'deepskyblue'}} icon="folder" />
+    <Avatar icon={<GithubIcon />}/>
+    <Avatar><img src="https://placeimg.com/80/80/animals"/></Avatar>
+    <Avatar title="Javier" img="https://placeimg.com/80/80/animals"/>
+    <Avatar style={{backgroundColor: 'yellowgreen'}}><GithubIcon /></Avatar>
   </section>
 );
 
