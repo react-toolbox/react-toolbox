bf68c83ea7da390e7f589cf845451f94ad71f3f3
Javi Velasco <javier.velasco86@gmail.com>
2015-11-27 18:32:19 +0100

    Allow img as children for avatar

--- spec/components/avatar.jsx
+++ spec/components/avatar.jsx
@@ -5,10 +5,11 @@ import GithubIcon from './github_icon';
 const AvatarTest = () => (
   <section>
     <h5>Avatars</h5>
-    <Avatar image={"https://placeimg.com/80/80/animals"} />
-    <Avatar title="Javier Velasco" image={"https://javivelasco.com/avatar404"} />
+    <Avatar image="https://placeimg.com/80/80/animals" />
+    <Avatar title="Javier Velasco" image="https://javivelasco.com/avatar404" />
     <Avatar icon="folder" />
     <Avatar><GithubIcon /></Avatar>
+    <Avatar><img src="https://placeimg.com/80/80/animals" /></Avatar>
   </section>
 );
 
