bf726ba088849f986d9328ce20566e134940ee7c
Javi Velasco <javier.velasco86@gmail.com>
2015-11-28 15:55:09 +0100

    Use Classnames in Link

--- components/link/Link.jsx
+++ components/link/Link.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
+import ClassNames from 'classnames';
 import style from './style';
 import FontIcon from '../font_icon';
 
 const Link = (props) => {
-  let className = style.root;
-  if (props.className) className += ` ${props.className}`;
-
+  const className = ClassNames(style.root, props.className);
   return (
-    <a
-      {...props}
-      data-react-toolbox='link'
-      className={className}
-    >
+    <a {...props} data-react-toolbox='link'className={className}>
       {props.icon ? <FontIcon className={style.icon} value={props.icon} /> : null}
       {props.label ? <abbr>{props.label}</abbr> : null}
       {props.count && parseInt(props.count) !== 0 ? <small>{props.count}</small> : null}
