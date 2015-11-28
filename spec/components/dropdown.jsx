ad916fba6d5758db5e3a1c805e856e02c9f17196
Javi Velasco <javier.velasco86@gmail.com>
2015-11-28 15:49:05 +0100

    Remove flex attributes styling

--- spec/components/dropdown.jsx
+++ spec/components/dropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dropdown from '../../components/dropdown';
+import style from '../style';
 
 const countries = [
   { value: 'EN-gb', label: 'England', img: 'http://' },
@@ -22,17 +23,10 @@ class DropdownTest extends React.Component {
   };
 
   customDropdownItem (data) {
-    const style = {
-      width: 32,
-      height: 32,
-      backgroundColor: '#ccc',
-      marginRight: 8
-    };
-
     return (
-      <div data-flex="horizontal grow" data-flex-content="center">
-        <img src={data.img} data-flex-grow="min" style={style} />
-        <div data-flex-grow="max" data-flex="vertical" >
+      <div className={style.dropdownTemplate}>
+        <img className={style.dropdownTemplateImage} src={data.img} />
+        <div className={style.dropdownTemplateContent}>
           <strong>{data.label}</strong>
           <small>{data.value}</small>
         </div>
