a6e162ec9e1223626ec5169e104d9077b7fc39f5
Javi Velasco <javier.velasco86@gmail.com>
2016-04-10 11:38:42 +0200

    Update documentation dependencies

--- docs/app/components/layout/main/components/navigation.jsx
+++ docs/app/components/layout/main/components/navigation.jsx
@@ -1,53 +1,41 @@
-import React from 'react';
-import { History } from 'react-router';
+import React, { PropTypes } from 'react';
 import { List, ListItem } from 'react-toolbox';
+import classnames from 'classnames';
 import components from '../modules/components';
 import style from './navigation.scss';
 
-const MainNavigation = React.createClass({
-  propTypes: {
-    active: React.PropTypes.bool,
-    className: React.PropTypes.string
-  },
-
-  mixins: [History],
-
-  renderDrawerItems () {
-    return Object.keys(components).map((key) => {
-      const ToolboxComponent = components[key];
-      const to = this.context.history.createHref(ToolboxComponent.path);
-      let className = style.item;
-      if (this.context.history.isActive(ToolboxComponent.path)) {
-        className += ` ${style.active}`;
-      }
-
-      return (
-        <ListItem
-          key={key}
-          caption={ToolboxComponent.name}
-          className={className}
-          selectable
-          to={to}
-        />
-      );
-    });
-  },
-
-  render () {
-    let className = style.root;
-    if (this.props.className) className += ` ${this.props.className}`;
-
+const MainNavigation = ({ className }, { router }) => {
+  const drawerItems = Object.keys(components).map((key) => {
+    const isActive = router.isActive(components[key].path);
     return (
-      <aside className={className}>
-        <List className={style.list} selectable ripple>
-          {this.renderDrawerItems()}
-        </List>
-        <footer className={style.footer}>
-          <span>React Toolbox © 2015</span>
-        </footer>
-      </aside>
+      <ListItem
+        key={key}
+        caption={components[key].name}
+        className={classnames(style.item, { [style.active]: isActive })}
+        selectable
+        onClick={() => { router.push(components[key].path);}}
+      />
     );
-  }
-});
+  });
+
+  return (
+    <aside className={classnames(style.root, { [className]: className })}>
+      <List className={style.list} selectable ripple>
+        {drawerItems}
+      </List>
+      <footer className={style.footer}>
+        <span>React Toolbox © 2016</span>
+      </footer>
+    </aside>
+  );
+};
+
+MainNavigation.propTypes = {
+  className: PropTypes.string
+};
+
+MainNavigation.contextTypes = {
+  router: PropTypes.object
+};
 
 export default MainNavigation;
