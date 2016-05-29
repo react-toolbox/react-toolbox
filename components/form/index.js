27962e8effaa31a05016550b904f845b6c6db828
Javi Velasco <javier.velasco86@gmail.com>
2016-05-29 19:46:16 +0200

    Inject dependencies for Form component

--- components/form/index.js
+++ components/form/index.js
@@ -1,1 +1,19 @@
-export default from './Form';
+import { formFactory } from './Form.js';
+import Autocomplete from '../autocomplete';
+import Button from '../button';
+import Checkbox from '../checkbox';
+import DatePicker from '../date_picker';
+import Dropdown from '../dropdown';
+import Input from '../input';
+import RadioGroup from '../radio';
+import Slider from '../slider';
+import Switch from '../switch';
+import TimePicker from '../time_picker';
+
+const ThemedForm = formFactory(
+  Autocomplete, Button, Checkbox, DatePicker, Dropdown,
+  Input, RadioGroup, Slider, Switch, TimePicker
+);
+
+export default ThemedForm;
+export { ThemedForm as Form };
