f0176107097c9220cc19ba72cbe78d9b2f4f3dec
John Bicknell <john@geckotec.co.uk>
2016-06-02 18:40:38 +0100

    Y for X

--- components/utils/events.js
+++ components/utils/events.js
@@ -2,14 +2,14 @@ export default {
   getMousePosition (event) {
     return {
       x: event.pageX - (window.scrollX || window.pageXOffset),
-      y: event.pageY - (window.scrollY || window.pageXOffset)
+      y: event.pageY - (window.scrollY || window.pageYOffset)
     };
   },
 
   getTouchPosition (event) {
     return {
       x: event.touches[0].pageX - (window.scrollX || window.pageXOffset),
-      y: event.touches[0].pageY - (window.scrollY || window.pageXOffset)
+      y: event.touches[0].pageY - (window.scrollY || window.pageYOffset)
     };
   },
 
