96599d3a0938647e9281a147b8fc4ee25ea57235
Javi Velasco <javier.velasco86@gmail.com>
2016-06-04 16:36:17 +0200

    Move some mixins

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true,
-	publicPath: config.output.publicPath,
+  noInfo: true,
+  publicPath: config.output.publicPath,
   stats: {
     colors: true
   }
@@ -17,14 +17,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, './spec/index.html'));
+  res.sendFile(path.join(__dirname, './spec/index.html'));
 });
 
 app.listen(8080, '0.0.0.0', function (err) {
-	if (err) {
-		console.log(err);
-		return;
-	}
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-	console.log('Listening at http://0.0.0.0:8080');
+  console.log('Listening at http://0.0.0.0:8080');
 });
