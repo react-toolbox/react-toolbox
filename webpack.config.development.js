fa35b84709e9f4608e023f907dc9400b74b118bc
Javi Velasco <javier.velasco86@gmail.com>
2016-07-11 21:47:25 +0200

    Add postcss-next postcss-include and reporter

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,7 +1,6 @@
 const pkg = require('./package');
 const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
@@ -17,22 +16,35 @@ module.exports = {
     publicPath: '/build/'
   },
   resolve: {
-    extensions: ['', '.scss', '.js', '.json'],
+    extensions: ['', '.css', '.scss', '.js', '.json'],
     packageMains: ['browser', 'web', 'browserify', 'main', 'style']
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: [/(node_modules)/, /react-css-themr/]
-      }, {
-        test: /\.(scss|css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
-      }
-    ]
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      include: [path.join(__dirname, './components'), path.join(__dirname, './spec')]
+    }, {
+      test: /\.css$/,
+      include: /node_modules/,
+      loaders: ['style-loader', 'css-loader']
+    }, {
+      test: /\.(scss|css)$/,
+      include: [path.join(__dirname, './components'), path.join(__dirname, './spec')],
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
+    }]
   },
-  postcss: [autoprefixer],
+  postcss (webpackInstance) {
+    return [
+      require('postcss-import')({
+        addDependencyTo: webpackInstance,
+        root: __dirname,
+        path: [path.join(__dirname, './components')]
+      }),
+      require('postcss-cssnext')(),
+      require('postcss-reporter')({ clearMessages: true })
+    ];
+  },
   plugins: [
     new ExtractTextPlugin('spec.css', { allChunks: true }),
     new webpack.HotModuleReplacementPlugin(),
