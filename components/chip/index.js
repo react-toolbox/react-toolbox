d3520d768f71c6c35dbeffcb8774a9298c0be692
Javi Velasco <javier.velasco86@gmail.com>
2016-07-13 10:36:03 +0200

    Migrate Chip style to PostCSS

--- components/chip/index.js
+++ components/chip/index.js
@@ -2,7 +2,7 @@ import { CHIP } from '../identifiers.js';
 import { themr } from 'react-css-themr';
 import { chipFactory } from './Chip.js';
 import Avatar from '../avatar';
-import theme from './theme.scss';
+import theme from './theme.css';
 
 const Chip = chipFactory(Avatar);
 const ThemedChip = themr(CHIP, theme)(Chip);
