ae11fae4a81872f5e364043ae090e670f65b6308
Javi Velasco <javier.velasco86@gmail.com>
2016-07-16 20:13:18 +0200

    Migrate Drawer style to PostCSS

--- components/drawer/index.js
+++ components/drawer/index.js
@@ -2,7 +2,7 @@ import { themr } from 'react-css-themr';
 import { DRAWER } from '../identifiers.js';
 import { Overlay } from '../overlay';
 import { drawerFactory } from './Drawer.js';
-import theme from './theme.scss';
+import theme from './theme.css';
 
 const Drawer = drawerFactory(Overlay);
 const ThemedDrawer = themr(DRAWER, theme)(Drawer);
