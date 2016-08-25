e2ed42bd3b57fce28cbc75ea0245220c1cb212f9
Andrew Sprouse <sprouse.a@gmail.com>
2016-08-25 11:27:02 -0400

    Remove overly strict children propType validation. fixes #641

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -9,42 +9,11 @@ const Layout = ({ className, children, theme }) => (
   </div>
 );
 
-const ALLOWED_THEMED = [
-  'Themed Panel',
-  'Themed NavDrawer|Themed Panel',
-  'Themed NavDrawer|Themed Panel|Themed Sidebar',
-  'Themed Panel|Themed Sidebar'
-];
-
-function getChildName (child) {
-  if (child.type) {
-    return child.type.displayName || child.type.name || child.type;
-  }
-  if (!child.constructor || !child.constructor.name) {
-    return 'UNKNOWN';
-  }
-  return child.constructor.name;
-}
-
 Layout.propTypes = {
-  children (props, propName, componentName) {
-    // Accept only [NavDrawer]Pane[Sidebar]
-    const children = props[propName];
-    if (React.Children.count(children) > 3) {
-      return new Error(
-        '`' + componentName + '` '
-        + 'should have a Pane for a child, optionally preceded and/or followed by a Drawer.'
-      );
-    }
-
-    const names = React.Children.map(children, getChildName).join('|');
-    if (!(~ALLOWED_THEMED.indexOf(names))) {
-      return new Error(
-        '`' + componentName + '` '
-        + 'should have a Panel for a child, optionally preceded by a NavDrawer and/or followed by a Sidebar.'
-      );
-    }
-  },
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element
+  ]),
   className: PropTypes.string,
   theme: PropTypes.shape({
     layout: PropTypes.string
