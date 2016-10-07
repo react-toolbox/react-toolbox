fc90df5f39013aab26291238dfeb22a27d52e6a0
Q S <quentin.serrurier@leadingtechnology.com>
2016-10-07 15:03:00 +0000

    lint no-var

--- components/table/TableRow.js
+++ components/table/TableRow.js
@@ -54,7 +54,7 @@ const factory = (Checkbox) => {
         return this.renderInput(key, value);
       } else if (value) {
         if (this.props.model[key].render) {
-          var disabled = true;
+          const disabled = true;
           return this.props.model[key].render(this.props.index, key, value, disabled);
         }
         return value.toString();
@@ -65,7 +65,7 @@ const factory = (Checkbox) => {
       const index = this.props.index;
       const inputType = utils.inputTypeForPrototype(this.props.model[key].type);
       if (this.props.model[key].render) {
-          var disabled = false;
+          const disabled = false;
           return this.props.model[key].render(index, key, value, disabled);
       }
       const inputValue = utils.prepareValueForInput(value, inputType);
