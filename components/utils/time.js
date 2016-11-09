adf3567b18ad32450f6e424a5165bbafc4fe99d3
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2016-11-09 23:37:34 +0100

    Fix linter issue with trailing comma in time utils

--- components/utils/time.js
+++ components/utils/time.js
@@ -4,7 +4,7 @@ const dateLocales = {
     monthsShort: 'Jan_Feb_März_Apr_Mai_Juni_Juli_Aug_Sept_Okt_Nov_Dez'.split('_'),
     weekdays: 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
     weekdaysShort: 'So_Mo_Di_Mi_Do_Fr_Sa'.split('_'),
-    weekdaysLetter: 'S_M_D_M_D_F_S'.split('_'),
+    weekdaysLetter: 'S_M_D_M_D_F_S'.split('_')
   },
   en: {
     months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
