7eb6680998a794658c1142f84e94ca8744755c9d
Kristoffer Rene Eckhoff <kristoffereckhoff@gmail.com>
2016-11-11 14:56:40 +0100

    Add no localization
    Based on moment.js norwegian time localization:
    https://github.com/moment/moment/blob/develop/locale/nb.js

--- components/utils/time.js
+++ components/utils/time.js
@@ -1,4 +1,11 @@
 const dateLocales = {
+   no: {
+    months: 'januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember'.split('_'),
+    monthsShort: 'jan._feb._mars_april_mai_juni_juli_aug._sep._okt._nov._des.'.split('_'),
+    weekdays: 'søndag_mandag_tirsdag_onsdag_torsdag_fredag_lørdag'.split('_'),
+    weekdaysShort: 'sø._ma._ti._on._to._fr._lø'.split('_'),
+    weekdaysLetter: []
+  },
   en: {
     months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
     monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
