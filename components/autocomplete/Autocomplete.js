430da22ff92fdbb8936ef16947a41f18bdcd4810
Danko Stojanovic <dstojanovic@fischerhomes.com>
2016-11-30 17:57:42 +0000

    Removes onQueryChange from Input props in Autocomplete
    PR #991 introduced the onQueryChange callback property for Autocomplete; however onQueryChange was spread into the Input component which raised a warning regarding onQueryChange not being a property of <input/>.
    
    Signed-off-by: Tom Dunlap, Zeno Su <tom@cando.com, zeno@cando.com>

--- components/autocomplete/Autocomplete.js
+++ components/autocomplete/Autocomplete.js
@@ -288,8 +288,8 @@ const factory = (Chip, Input) => {
 
    render () {
      const {
-      allowCreate, error, label, source, suggestionMatch, //eslint-disable-line no-unused-vars
-      selectedPosition, showSuggestionsWhenValueIsSet,    //eslint-disable-line no-unused-vars
+      allowCreate, error, label, onQueryChange, selectedPosition,  //eslint-disable-line no-unused-vars
+      showSuggestionsWhenValueIsSet, source, suggestionMatch,      //eslint-disable-line no-unused-vars
       theme, ...other
     } = this.props;
      const className = classnames(theme.autocomplete, {
