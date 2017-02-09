22cdaa421b5df280737d1c54d695bf17e5d04b04
Javi Velasco <javier.velasco86@gmail.com>
2017-02-09 14:38:02 +0100

    Smaller CSSModules Button

--- src/css-modules/Button/index.js
+++ src/css-modules/Button/index.js
@@ -9,26 +9,20 @@ import ButtonNode from './ButtonNode';
 import LinkNode from './LinkNode';
 import theme from './Button.css';
 
-const ripple = withRippleFactory({
-  RippleNode: createComponent(RippleNode, {
-    modifiers: ['floating'],
-  }),
-  RippleWrapper: createComponent(RippleWrapper, {
-    modifiers: ['floating'],
-  }),
-  passthrough: props => ({
-    floating: props.floating,
-    theme: getNamespacedTheme('ripple', props.theme),
-  }),
-})();
-
-const Button = buttonFactory({
-  ripple,
+export default themr('RTButton', theme)(buttonFactory({
+  passthrough: ['theme'],
   LinkNode,
   ButtonNode,
-  passthrough: props => ({
-    theme: props.theme,
-  }),
-});
-
-export default themr('RTButton', theme)(Button);
+  ripple: withRippleFactory({
+    passthrough: props => ({
+      floating: props.floating,
+      theme: getNamespacedTheme('ripple', props.theme),
+    }),
+    RippleNode: createComponent(RippleNode, {
+      modifiers: ['floating'],
+    }),
+    RippleWrapper: createComponent(RippleWrapper, {
+      modifiers: ['floating'],
+    }),
+  })(),
+}));
