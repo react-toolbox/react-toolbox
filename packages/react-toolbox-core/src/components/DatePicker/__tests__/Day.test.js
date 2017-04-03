e455f3e365eebed77c5d6495e9782f0907ec2436
Javi Velasco <javier.velasco86@gmail.com>
2017-04-03 17:42:15 +0200

    Add Day tests

--- packages/react-toolbox-core/src/components/DatePicker/__tests__/Day.test.js
+++ packages/react-toolbox-core/src/components/DatePicker/__tests__/Day.test.js
@@ -4,6 +4,7 @@ import { shallow } from 'enzyme';
 import createElement from '../../../utils/createElement';
 
 describe('<Day />', () => {
+  const day = new Date(2017, 2, 1);
   let mockIsToday;
   let dayFactory;
   let DayNode;
@@ -27,7 +28,6 @@ describe('<Day />', () => {
     });
 
     it('passes today prop as true', () => {
-      const day = new Date(2017, 2, 1);
       const wrapper = shallow(<Day day={day} />);
       const isToday = wrapper.find(DayNode).prop('today');
       expect(isToday).toEqual(true);
@@ -44,12 +44,78 @@ describe('<Day />', () => {
       Day = dayFactory({ DayNode });
     });
 
-    it('renders a simple Day', () => {
+    it('passes today prop as false', () => {
       const day = new Date(2017, 2, 1);
       const wrapper = shallow(<Day day={day} />)
       const isToday = wrapper.find(DayNode).prop('today');
       expect(isToday).toEqual(false);
       expect(mockIsToday).toHaveBeenCalledWith(day);
+    });
+
+    it('passes disabled when day is disabled', () => {
+      const isDayDisabled = jest.fn(() => true);
+      const wrapper = shallow(
+        <Day
+          day={day}
+          isDayDisabled={isDayDisabled}
+          viewDate={day}
+        />
+      );
+      const disabled = wrapper.find(DayNode).prop('disabled');
+      expect(disabled).toEqual(true);
+      expect(isDayDisabled).toHaveBeenCalledWith(day);
+    });
+
+    it('passes highlighted when day is in highlighted range', () => {
+      const highlighted = { from: new Date(2017, 1, 15), to: new Date(2017, 2, 15) };
+      const wrapper = shallow(
+        <Day
+          day={day}
+          highlighted={highlighted}
+          viewDate={day}
+        />
+      );
+      const isHighlighted = wrapper.find(DayNode).prop('highlighted');
+      expect(isHighlighted).toEqual(true);
+    });
+
+    it('passes inRange when day is in selected range', () => {
+      const selected = { from: new Date(2017, 1, 15), to: new Date(2017, 2, 15) };
+      const wrapper = shallow(
+        <Day
+          day={day}
+          selected={selected}
+          viewDate={day}
+        />
+      );
+      const inRange = wrapper.find(DayNode).prop('inRange');
+      expect(inRange).toEqual(true);
+    });
+
+    it('passes selected when day is selected in from', () => {
+      const selected = { from: new Date(2017, 1, 15), to: new Date(2017, 2, 15) };
+      const wrapper = shallow(
+        <Day
+          day={selected.from}
+          selected={selected}
+          viewDate={selected.from}
+        />
+      );
+      expect(wrapper.find(DayNode).prop('selected')).toEqual(true);
+      expect(wrapper.find(DayNode).prop('selectedSource')).toEqual('from');
+    });
+
+    it('passes selected when day is selected in to', () => {
+      const selected = { from: new Date(2017, 1, 15), to: new Date(2017, 2, 15) };
+      const wrapper = shallow(
+        <Day
+          day={selected.to}
+          selected={selected}
+          viewDate={selected.to}
+        />
+      );
+      expect(wrapper.find(DayNode).prop('selected')).toEqual(true);
+      expect(wrapper.find(DayNode).prop('selectedSource')).toEqual('to');
     });
   });
 });
