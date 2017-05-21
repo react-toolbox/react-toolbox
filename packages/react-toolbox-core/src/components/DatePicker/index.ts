862199db06d91cf87e2e390345656086b5ef0b3c
Javi Velasco <javier.velasco86@gmail.com>
2017-05-21 13:12:50 +0200

    Index date picker localisation functions

--- packages/react-toolbox-core/src/components/DatePicker/index.ts
+++ packages/react-toolbox-core/src/components/DatePicker/index.ts
@@ -1,5 +1,6 @@
+export * from './locale';
 export { default as datePickerFactory } from './DatePicker';
-export { default as dayFactory } from './Day';
-export { default as monthFactory } from './Month';
 export { default as rangePickerFactory } from './RangePicker';
 export { default as singlePickerFactory } from './SinglePicker';
+export { default as monthFactory } from './Month';
+export { default as dayFactory } from './Day';
---
+++ packages/react-toolbox-core/src/components/DatePicker/locale/index.ts
@@ -0,0 +1,4 @@
+export { default as dateLocales } from './dateLocales';
+export { default as getFullDayOfWeek } from './getFullDayOfWeek';
+export { default as getFullMonth } from './getFullMonth';
+export { default as getShortMonth } from './getShortMonth';
