431abb1bdedb5caf291d2a53df251cac35e34493
Tom Chen <developer@tomchentw.com>
2017-08-03 00:19:53 +0800

    Add workaround to `is-component-of-type` for `react-hot-loader@^3` (#1569)

--- components/utils/is-component-of-type.js
+++ components/utils/is-component-of-type.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 let customChecker;
 
 /**
@@ -16,6 +18,10 @@ export function overrideComponentTypeChecker(providedChecker) {
  * @param reactElement {ReactElement} - any React Element (not a real DOM node)
  */
 export function defaultChecker(classType, reactElement) {
+  if (process.env.NODE_ENV !== 'production') {
+    // https://github.com/gaearon/react-hot-loader/blob/v3.0.0-beta.7/docs/Known%20Limitations.md#checking-element-types
+    classType = React.createElement(classType).type;// eslint-disable-line no-param-reassign
+  }
   return reactElement && reactElement.type === classType;
 }
 
