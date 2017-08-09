6698cc2e99a36f28f0217ffd077326a0a4bd100c
Brent Smith <brent.smith@showings.com>
2017-08-09 10:18:15 -0500

    Rename roundTo to step and fix top of the hour issues.
    Renamed the roundTo prop on the TimePicker to be step.
    Fixed issues when going from minute 55 to 0 that would cause the hours to change.
    Fix the initial time so that it also is set to the nearest step.

--- components/time_picker/TimePicker.d.ts
+++ components/time_picker/TimePicker.d.ts
@@ -162,6 +162,10 @@ export interface TimePickerProps {
    */
   readonly?: boolean;
   /**
+   * Option to round to the given minute when picking a time
+   */
+  step?: number;
+  /**
    * Classnames object defining the component style.
    */
   theme?: TimePickerTheme;
@@ -173,10 +177,6 @@ export interface TimePickerProps {
    * Additional attributes passed to inner Input component.
    */
   [key: string]: any;
-  /**
-   * Option to round to the given minute when picking a time
-   */
-  roundTo?: number;
 }
 
 export class TimePicker extends React.Component<TimePickerProps, {}> { }
