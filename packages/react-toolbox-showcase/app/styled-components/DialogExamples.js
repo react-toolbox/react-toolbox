45cd102e200714b79caefa187b0fd8298673eca0
Javi Velasco <javier.velasco86@gmail.com>
2017-09-09 17:32:23 +0200

    Add DialogExamples to SC Showcase

---
+++ packages/react-toolbox-showcase/app/styled-components/DialogExamples.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Button from 'react-toolbox-sc/lib/Button';
+import Dialog, { DialogActions, DialogBody } from 'react-toolbox-sc/lib/Dialog';
+
+class DialogExamples extends Component {
+  state = {
+    active: false
+  }
+
+  handleToggle = () => {
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <h5>Dialog</h5>
+        <Button onClick={this.handleToggle}>Show it</Button>
+        <Dialog active={this.state.active}>
+          <DialogBody>
+            <h1>Use Google's location service?</h1>
+            <p>
+              Let Google help apps <strong>determine location</strong>.
+              This means sending anonymous location data to Google, even when no apps are running.
+            </p>
+          </DialogBody>
+          <DialogActions>
+            <Button primary onClick={this.handleToggle}>Disagree</Button>
+            <Button primary onClick={this.handleToggle}>Agree</Button>
+          </DialogActions>
+        </Dialog>
+      </section>
+    );
+  }
+}
+
+export default DialogExamples;
--- packages/react-toolbox-showcase/app/styled-components/index.js
+++ packages/react-toolbox-showcase/app/styled-components/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { injectGlobal } from 'styled-components';
 import ButtonExamples from './ButtonExamples';
 import DatePickerExamples from './DatePickerExamples';
+import DialogExamples from './DialogExamples';
+
+injectGlobal`
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+    text-size-adjust: 100%;
+    -webkit-touch-callout: none;
+  }
+`
 
 const StyledComponents = () => (
   <div>
     <ButtonExamples />
     <DatePickerExamples />
+    <DialogExamples />
   </div>
 );
 
