b2e38f002a03f5b871bdd6460b035741e9685846
Javi Velasco <javier.velasco86@gmail.com>
2017-09-22 13:25:17 +0200

    Add SC Avatar

---
+++ packages/react-toolbox-sc/src/Avatar/Avatar.js
@@ -0,0 +1,51 @@
+import avatarFactory from 'react-toolbox-core/lib/components/Avatar';
+import styled, { css } from 'styled-components';
+
+const Avatar = avatarFactory({
+  WrapperNode: styled.div`
+    background-color: rgb(158, 158, 158);
+    border-radius: 50%;
+    color: rgb(255, 255, 255);
+    display: inline-flex;
+    font-size: 24px;
+    height: 40px;
+    overflow: hidden;
+    position: relative;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
+    width: 40px;
+
+    & > img {
+      display: inline-block;
+      height: auto;
+      max-width: 100%;
+    }
+
+    & > svg {
+      fill: currentColor;
+      height: 40px;
+      width: 1em;
+    }
+
+    ${props =>
+      props.cover &&
+      props.image &&
+      css`
+        background-color: transparent;
+        background-image: url(${props.image});
+        background-position: center;
+        background-size: cover;
+      `};
+  `,
+  ImgNode: styled.img`
+    display: inline-block;
+    height: auto;
+    max-width: 100%;
+
+    ${props => props.cover && css`visibility: hidden;`};
+  `,
+});
+
+export default Avatar;
--- packages/react-toolbox-sc/src/index.js
+++ packages/react-toolbox-sc/src/index.js
@@ -1,3 +1,7 @@
+export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as DatePicker } from './DatePicker';
 export { default as withRipple } from './withRipple';
+export { default as List } from './List';
+export { ListItem } from './List';
+export { Divider } from './List';
