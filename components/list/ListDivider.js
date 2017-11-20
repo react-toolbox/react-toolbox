157c0cc56903f62be6acdca174cfa54ac6860de9
Anthony Crognale <anthony@knewton.com>
2017-11-20 11:30:34 -0500

    Make ListDivider hidden from screen reader

--- components/list/ListDivider.js
+++ components/list/ListDivider.js
@@ -4,7 +4,10 @@ import { themr } from 'react-css-themr';
 import { LIST } from '../identifiers';
 
 const ListDivider = ({ inset, theme }) => (
-  <hr className={inset ? `${theme.divider} ${theme.inset}` : theme.divider} />
+  <hr
+    className={inset ? `${theme.divider} ${theme.inset}` : theme.divider}
+    ariaHidden="true"
+  />
 );
 
 ListDivider.propTypes = {
