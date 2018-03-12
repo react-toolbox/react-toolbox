776bf2187fe57c0d8ab2b16bd211dc1801a262fa
Ben Eisenberg <ben.iceberg@gmail.com>
2018-03-12 11:49:27 +0100

    test: avatar add title test

--- components/avatar/__test__/index.spec.js
+++ components/avatar/__test__/index.spec.js
@@ -25,5 +25,12 @@ describe('Avatar', () => {
       const wrapper = shallow(<Avatar theme={theme} />);
       expect(wrapper.props().className).toBe('avatar');
     });
+
+    it('should show first letter of name if title is given', () => {
+      const wrapper = mount(<Avatar theme={theme} title="Ben" />);
+      const { className, children } = wrapper.find('span').props();
+      expect(className).toContain(theme.letter);
+      expect(children).toContain('B');
+    });
   });
 });
