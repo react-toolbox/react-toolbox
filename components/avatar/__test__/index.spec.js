3bd518ec0e93064f48ee91dd84e17f6b51397261
Ben Eisenberg <ben.iceberg@gmail.com>
2018-03-12 10:26:03 +0100

    test: avatar add tests

--- components/avatar/__test__/index.spec.js
+++ components/avatar/__test__/index.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { Avatar } from '../Avatar';
 import theme from '../theme.css';
 
@@ -9,6 +9,21 @@ describe('Avatar', () => {
     it('uses no cover as default', () => {
       const wrapper = mount(<Avatar theme={theme} />);
       expect(wrapper.props().cover).toBe(false);
+    });
+
+    it('uses no alternative as default', () => {
+      const wrapper = mount(<Avatar theme={theme} />);
+      expect(wrapper.props().alt).toBe('');
+    });
+
+    it('uses no theme as default', () => {
+      const wrapper = mount(<Avatar theme={theme} />);
+      expect(wrapper.props().theme).toMatchObject({});
+    });
+
+    it('has className `avatar`', () => {
+      const wrapper = shallow(<Avatar theme={theme} />);
+      expect(wrapper.props().className).toBe('avatar');
     });
   });
 });
