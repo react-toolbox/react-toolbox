1842a97963f2d7070562b9d28e6bda5710fc983e
Ben Eisenberg <ben.iceberg@gmail.com>
2018-03-13 10:24:48 +0100

    refactor: avatar deviding

--- components/avatar/__test__/index.spec.js
+++ components/avatar/__test__/index.spec.js
@@ -3,7 +3,6 @@ import { mount, shallow } from 'enzyme';
 import { Avatar } from '../Avatar';
 import theme from '../theme.css';
 
-
 describe('Avatar', () => {
   describe('#render', () => {
     it('uses no cover as default', () => {
@@ -25,8 +24,10 @@ describe('Avatar', () => {
       const wrapper = shallow(<Avatar theme={theme} />);
       expect(wrapper.props().className).toBe('avatar');
     });
+  });
 
-    it('should show first letter of name if title is given', () => {
+  describe('with title', () => {
+    it('should show first letter of name if image is not given', () => {
       const wrapper = mount(<Avatar theme={theme} title="Ben" />);
       const { className, children } = wrapper.find('span').props();
       expect(className).toContain(theme.letter);
