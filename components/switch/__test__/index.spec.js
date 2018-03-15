1b592fe6256a937fa52d9635cc1685d12624dfcd
georgeperry1 <gmperry18@gmail.com>
2018-03-15 10:13:45 +0100

    chore: removed console logs from test

--- components/switch/__test__/index.spec.js
+++ components/switch/__test__/index.spec.js
@@ -37,9 +37,7 @@ describe('Switch', () => {
   it('only selects a single value', () => {
     const wrapper = mount(<SwitchTest />);
     expect(wrapper.find(Switch).first().props().checked).toEqual(true);
-    console.log('WRAPPER BEFORE:', wrapper.find(Switch).first().props());
     wrapper.find(Switch).first().find('input').simulate('click');
-    console.log('WRAPPER AFTER:', wrapper.find(Switch).first().props());
     expect(wrapper.find(Switch).first().props().checked).toEqual(false);
   });
 });
