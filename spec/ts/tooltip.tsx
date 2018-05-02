7e8b582a5577c715e68700f5a9ae1186ad05b739
Keyur Patel <kepatel@shutterstock.com>
2018-05-02 09:49:34 -0400

    ts changes

--- spec/ts/tooltip.tsx
+++ spec/ts/tooltip.tsx
@@ -6,10 +6,10 @@ import Tooltip, { tooltipFactory } from '../../components/tooltip';
 import Chip from '../../components/chip';
 import Avatar from '../../components/avatar';
 
-const TooltipFontIcon = tooltipFactory({ passthrough: false })(FontIcon);
+const TooltipFontIcon = tooltipFactory({ passthrough: false, showCaret: false })(FontIcon);
 const TooltipButton = Tooltip(Button);
 const TooltipInput = Tooltip(Input);
-const TooltipStrong = Tooltip(({ children, ...other }) => {
+const TooltipStrong = Tooltip(({ children,...other }) => {
   delete other.theme;
   return <strong {...other}>{children}</strong>;
 });
@@ -20,29 +20,30 @@ const TooltipTest = () => (
   <section>
     <h5>Tooltip</h5>
     <p>Give information on :hover</p>
-    <TooltipButton label="Bookmark" icon="bookmark" raised primary tooltip="Bookmark Tooltip" tooltipDelay={1000} />
-    <TooltipButton icon="add" floating accent tooltip="Floating Tooltip" />
-    <TooltipButton icon="add" floating disabled tooltip="Floating can not be shown" />
+    <TooltipButton label="Bookmark" icon="bookmark" raised primary tooltip="Bookmark Tooltip" tooltipDelay={1000} showCaret={false} />
+    <TooltipButton icon="add" floating accent tooltip="Floating Tooltip" showCaret={false} />
+    <TooltipButton icon="add" floating disabled tooltip="Floating can not be shown" showCaret={false} />
     <TooltipButton
       icon="add"
       floating
       tooltip={<div><p>An example with</p><p>Multiline!</p></div>}
+      showCaret={false}
     />
-    <ChipTooltip tooltip="Dolor sit amet" tooltipPosition="top">
+    <ChipTooltip tooltip="Dolor sit amet" tooltipPosition="top" showCaret={false}>
       <Avatar icon="home" />
       <span>Tooltip in a chip</span>
     </ChipTooltip>
-    <TooltipInput tooltip="lorem ipsum..." />
-    <p>Lorem ipsum dolor sit amet, <TooltipStrong tooltip="This is a auto show tooltip">consectetur</TooltipStrong> adipiscing elit.</p>
+    <TooltipInput tooltip="lorem ipsum..." showCaret={false}/>
+    <p>Lorem ipsum dolor sit amet, <TooltipStrong tooltip="This is a auto show tooltip" showCaret={false}>consectetur</TooltipStrong> adipiscing elit.</p>
     <p>
       Click this next word to show and hide on click:
       {' '}
-      <TooltipStrongDirect tooltip="This is a auto show tooltip" tooltipShowOnClick>
+      <TooltipStrongDirect tooltip="This is a auto show tooltip" tooltipShowOnClick showCaret={false}>
         oh hai
       </TooltipStrongDirect>
       {' '}. This is useful for mobile!
     </p>
-    <TooltipFontIcon value="code" tooltip="This is a test with FontIcon" />
+    <TooltipFontIcon value="code" tooltip="This is a test with FontIcon" showCaret={false} />
     <br />
     Using carets in tooltip:
     <br />
