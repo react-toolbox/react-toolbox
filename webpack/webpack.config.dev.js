6e38a732fc1cfdd05239fb67abba7924fc0c35a2
Matthew Rumery <matt.superman@gmail.com>
2018-10-19 17:47:19 -0600

    upgrade to babel 7, upgrade gulp-babel & babel-loader to work with babel 7

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    'babel-polyfill',
+    '@babel/polyfill',
     './spec/index.js'
   ],
   output: {
